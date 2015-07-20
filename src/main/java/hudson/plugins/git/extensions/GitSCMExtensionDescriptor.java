a37b3748832f07c6daa37f3a64696881df4b9207
Mark Waite <mark.earl.waite@gmail.com>
2015-07-20 06:51:49 -0600

    Return an extension list from GitSCMExtensionDescriptor even if jenkins == null
    Fix findbugs warning

--- src/main/java/hudson/plugins/git/extensions/GitSCMExtensionDescriptor.java
+++ src/main/java/hudson/plugins/git/extensions/GitSCMExtensionDescriptor.java
@@ -3,25 +3,22 @@ package hudson.plugins.git.extensions;
 import hudson.DescriptorExtensionList;
 import hudson.model.Descriptor;
 import hudson.plugins.git.GitSCM;
-import java.util.logging.Logger;
 import jenkins.model.Jenkins;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public abstract class GitSCMExtensionDescriptor extends Descriptor<GitSCMExtension> {
-    private static final Logger LOGGER = Logger.getLogger(GitSCMExtensionDescriptor.class.getName());
-
     public boolean isApplicable(Class<? extends GitSCM> type) {
         return true;
     }
 
     public static DescriptorExtensionList<GitSCMExtension,GitSCMExtensionDescriptor> all() {
-        Jenkins jenkins = Jenkins.getInstance();
-        if (jenkins == null) {
-            LOGGER.severe("Jenkins instance is null in GitSCMExtensionDescriptor");
-            return null;
+        final Jenkins jenkins = Jenkins.getInstance();
+        if (jenkins != null) {
+            return jenkins.getDescriptorList(GitSCMExtension.class);
+        } else {
+            return DescriptorExtensionList.createDescriptorList((Jenkins)null, GitSCMExtension.class);
         }
-        return jenkins.getDescriptorList(GitSCMExtension.class);
     }
 }
